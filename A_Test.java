542ba614350bb86434c2e0dc784aecc7931478ba
kc3vkr <kc3.vkr@gmail.com>
2018-07-04 16:26:23 +0530

    added A and E .java files

---
+++ A_Test.java
@@ -0,0 +1,8 @@
+class A_Test
+{
+  public static void main(String[] args)
+   {
+     system.out.println("Welcome to git hub");
+    }
+
+}
---
+++ E_Test.java
@@ -0,0 +1,7 @@
+class E_Test
+{
+ public static void main(String[] args)
+ {
+  System.out.println("Welcome");
+  }
+}
